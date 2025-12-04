792d9d99da01af19b910845375b5365bb44ea4bf
Chengyu Li <1546951229@qq.com>
2025-12-04 12:12:06 +0800

    Add Part5 hy-notes-frontend project

--- Part5/bloglist-frontend/src/services/login.js
+++ Part5/bloglist-frontend/src/services/login.js
@@ -2,8 +2,11 @@ import axios from 'axios'
 const baseUrl = '/api/login'
 
 const login = async credentials => {
-    const response = await axios.post(baseUrl, credentials)
-    return response.data
+  const response = await axios.post(baseUrl, credentials)
+  //调用后端/api/login 的post
+  return response.data
 }
 
 export default { login }
+
+
